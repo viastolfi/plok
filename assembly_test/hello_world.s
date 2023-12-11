637a66db482fd1f7e3a2bbf20c89881d665c8e06
vincentastolfi <vincent.astolfi@etu.uca.fr>
2023-12-11 14:51:38 +0100

    :sparkles: Hello world asm add

---
+++ assembly_test/hello_world.s
@@ -0,0 +1,20 @@
+.global _main
+.align 2 
+
+_main:
+    b _hello_world
+    b _terminate
+_hello_world:
+    mov X0, #1
+    adrp X1, hello_txt@PAGE
+    add X1, X1, hello_txt@PAGEOFF
+    mov X2, #14
+    mov X16, #4
+    svc 128
+
+_terminate:
+    mov x0, 0
+    mov x16, 1
+    svc 128
+.data
+hello_txt: .ascii "Hello World !\n"
