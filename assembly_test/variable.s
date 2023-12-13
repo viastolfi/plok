9b806910795559a989625623641bb7250071aba4
vincentastolfi <vincent.astolfi@etu.uca.fr>
2023-12-13 12:51:38 +0100

    :sparkles: Variable creation in assembly

---
+++ assembly_test/variable.s
@@ -0,0 +1,12 @@
+.global _main
+.align 2
+
+_main:
+    sub sp, sp, (8 * 14) // Allocate space for the whole block
+    mov x1, 42 // Add value 4 on the x1 register
+    str x1, [sp, (8 * 11)] // write the value of x1 to slot 11
+    ldr x0, [sp, (8 * 11)] // read from slot 11 into x0
+    add sp, sp, (8 * 14) // Free the space at the end of the block
+
+    mov x16, 1
+    svc 128
