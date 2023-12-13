0b5ac544d5d7043fac2182c1acef2d6d990cbb4d
vincentastolfi <vincent.astolfi@etu.uca.fr>
2023-12-13 23:33:09 +0100

    :memo: update readme with docs I use

--- assembly_test/variable.s
+++ assembly_test/variable.s
@@ -2,11 +2,11 @@
 .align 2
 
 _main:
-    sub sp, sp, (8 * 14) // Allocate space for the whole block
+    sub sp, sp, 32 // Allocate space for the whole block
     mov x1, 42 // Add value 4 on the x1 register
-    str x1, [sp, (8 * 11)] // write the value of x1 to slot 11
-    ldr x0, [sp, (8 * 11)] // read from slot 11 into x0
-    add sp, sp, (8 * 14) // Free the space at the end of the block
+    str x1, [sp, 11] // write the value of x1 to slot 11
+    ldr x0, [sp, 11] // read from slot 11 into x0
+    add sp, sp, 32 // Free the space at the end of the block
 
     mov x16, 1
     svc 128
